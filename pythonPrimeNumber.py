69f4e8238ccf8babbfcd3fa27341b9c6ae1d972f
bilgeersayin180 <55208069+bilgeersayin180@users.noreply.github.com>
2020-08-04 21:11:52 +0300

    Update pythonPrimeNumber.py

--- pythonPrimeNumber.py
+++ pythonPrimeNumber.py
@@ -1,20 +1,7 @@
-def isPrimeNumber(num):
-    if (num == 2):
-        print(num,"is a prime number")
-        exit
-    limit = num 
-    numList = []
-    for i in range(0,limit):
-        numList.append(i)
-    print(numList)
-    for j in range(2,limit):
-        if (num % numList[j] == 0):
-            print(num,"is not a prime number")
-            break
-        print(j)
-        if (j == limit - 1):
-            print(num,"is a prime number")
-        
-
+def isPrime(num):
+    for i in range(2, num):
+        if (num % i == 0):
+            return False
+    return True
 number = int(input("Enter a positive integer: "))
-print(isPrimeNumber(number))
+print(isPrime(number))
